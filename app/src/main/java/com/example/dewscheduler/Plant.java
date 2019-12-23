931cbd7dc5a581c0f380b19c3b7aed0ddd52a990
Dmitry Sologub <ddsologub@gmail.com>
2019-12-23 15:05:15 +0700

    Add watering times

--- app/src/main/java/com/example/dewscheduler/Plant.java
+++ app/src/main/java/com/example/dewscheduler/Plant.java
@@ -1,20 +1,43 @@
 package com.example.dewscheduler;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Plant {
     private String title;
     private String description;
     private int number;
     private int icon;
+    private LocalDateTime plantDate;
+    private ArrayList<LocalDateTime> wateringDates = new ArrayList<>();
 
-    public Plant (){
+    public Plant() {
         //empty but needed
     }
 
-    public Plant (String title, String description, int number, int icon){
+    public Plant(String title, String description, int number, int icon, LocalDateTime plantDate) {
         this.title = title;
         this.description = description;
         this.number = number;
         this.icon = icon;
+        this.plantDate = plantDate;
+        this.wateringDates.add(plantDate.plusDays(number));
+    }
+
+
+    public LocalDateTime getWateringDate() {
+        if (wateringDates.size() > 0)
+            return wateringDates.get(wateringDates.size() - 1);
+        else
+            return LocalDateTime.now().plusDays(number);
+    }
+
+    public int getWateringLevel() {
+        LocalDateTime last = getWateringDate();
+        LocalDateTime cur = LocalDateTime.now();
+        long days = Duration.between(cur, last).toDays();
+        return (int) ((float)days / number) * 5;
     }
 
     public String getTitle() {
@@ -32,4 +55,5 @@ public class Plant {
     public int getIcon() {
         return icon;
     }
+
 }
